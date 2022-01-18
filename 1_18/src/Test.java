ba001272f7c176eac76d30661111d8cce5b9b0a0
申金旺 <2972520044@qq.com>
2022-01-18 19:08:05 +0800

    1_18

--- 1_18/src/Test.java
+++ 1_18/src/Test.java
@@ -10,7 +10,62 @@ import java.util.Queue;
  * @date 2022/1/18 8:52
  * @Version 1.0
  */
+class MyStack {
+    //请你仅使用两个队列实现一个后入先出（LIFO）的栈，并支持普通栈的全部四种操作（push、top、pop 和 empty）。
+    //
+    //实现 MyStack 类：
+    //
+    //    void push(int x) 将元素 x 压入栈顶。
+    //    int pop() 移除并返回栈顶元素。
+    //    int top() 返回栈顶元素。
+    //    boolean empty() 如果栈是空的，返回 true ；否则，返回 false 。
+    //
+    //
+    //
+    //注意：
+    //
+    //    你只能使用队列的基本操作 —— 也就是 push to back、peek/pop from front、size 和 is empty 这些操作。
+    //    你所使用的语言也许不支持队列。 你可以使用 list （列表）或者 deque（双端队列）来模拟一个队列 , 只要是标准的队列操作即可。
+    //
+    //来源：力扣（LeetCode）
+    //链接：https://leetcode-cn.com/problems/implement-stack-using-queues
+    //著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 
+    Queue<Integer> queue1;
+    Queue<Integer> queue2;
+
+    /** Initialize your data structure here. */
+    public MyStack() {
+        queue1 = new LinkedList<Integer>();
+        queue2 = new LinkedList<Integer>();
+    }
+
+    /** Push element x onto stack. */
+    public void push(int x) {
+        queue2.offer(x);
+        while (!queue1.isEmpty()) {
+            queue2.offer(queue1.poll());
+        }
+        Queue<Integer> temp = queue1;
+        queue1 = queue2;
+        queue2 = temp;
+    }
+
+    /** Removes the element on top of the stack and returns that element. */
+    public int pop() {
+        return queue1.poll();
+    }
+
+    /** Get the top element. */
+    public int top() {
+        return queue1.peek();
+    }
+
+    /** Returns whether the stack is empty. */
+    public boolean empty() {
+        return queue1.isEmpty();
+    }
+}
 public class Test {
     /*int count = 0;
     int size(TreeNode root){//节点个数
