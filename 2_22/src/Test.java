af62c3a569cf5bd1e07c4d589604a9dfee370786
申金旺 <2972520044@qq.com>
2022-02-22 21:45:11 +0800

    2_22

---
+++ 2_22/src/Test.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+/**
+ * @ClassName $申先生
+ * @Description days
+ * @date 2022/2/22 18:39
+ * @Version 1.0
+ */
+
+public class Test {
+    public static void main(String[] args) {
+        //咦，今天竟然没敲代码，可恶呀，再不济我也要敲一个hello world!!!!!哼！！！
+        System.out.println("hello world");
+    }
+}
