ecb9d647cc963c6e2cf37c1a14ed54dc7532eaa1
申金旺 <2972520044@qq.com>
2022-03-01 21:56:37 +0800

    2_26

--- 2_26/src/HashBuck.java
+++ 2_26/src/HashBuck.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.HashMap;
+
 /**
  * @ClassName $申先生
  * @Description days
@@ -7,9 +10,9 @@
 
 public class HashBuck {
     public final double load_factor = 0.75;
-    Node hash[] = new Node[8];
+
     static int usedSize = 0;
-    class Node{
+    static class Node{
         int key;
         int val;
 
@@ -20,27 +23,79 @@ public class HashBuck {
             this.val = val;
         }
     }
+    Node[] hash ;
+    public HashBuck(){
+        this.hash = new Node[10];
+    }
 
-    public void put(int key , int val){
+    public void put(int key , int val) {
         int index = key % hash.length;
-        Node node = new Node(key,val);
+        Node node = new Node(key, val);
         Node ret = hash[index];
-        while(ret != null){
-            if(ret.key == key){
+        while (ret != null) {
+            //覆盖重复元素
+            if (ret.key == key) {
                 ret.val = val;
             }
             ret = ret.next;
         }
-        if(hash[index] == null){
-            hash[index] = node;
-        }else{
-            Node cur = hash[index];
-            node.next = cur;
-            node = hash[index];
+        node.next = hash[index];
+        hash[index] = node;
+        usedSize++;
+
+        if(loadFactor() >= load_factor){//负载因子超出限制，扩容
+            resize();
         }
     }
 
+    private void resize(){
+        //2倍扩容
+        int size = hash.length;
+        Node cur[] = new Node[size * 2];
+        //重新哈希
+        for(int i = 0 ; i < size ; i++ ){
+            Node ret = hash[i];
+            while(ret != null){
+                int index = ret.key % cur.length;
+                //这个位置不止一个元素，采用头插法
+                //先记录第二个元素防止丢失
+                Node retNext = ret.next;
 
+                ret.next = cur[index];
+                cur[index] = ret;
+                ret = retNext;
+            }
+        }
+        hash = cur;
+    }
 
+    public int get(int key){
+        int index = key % hash.length;
+        Node ret = hash[index];
+        while(ret != null){
+            if(ret.key == key){
+                return ret.val;
+            }
+            ret = ret.next;
+        }
+        return -1;
+    }
+
+    private double loadFactor(){
+        return usedSize * 1.0 / hash.length;
+    }
+
+    public static void main(String[] args) {
+        HashBuck hashBuck = new HashBuck();
+        hashBuck.put(1,1);
+        hashBuck.put(12,12);
+        hashBuck.put(3,3);
+        hashBuck.put(4,4);
+        hashBuck.put(2,2);
+        hashBuck.put(6,6);
+        //hashBuck.put(11,11);
+        //hashBuck.put(8,8);
+        System.out.println(hashBuck);
+    }
 
 }
