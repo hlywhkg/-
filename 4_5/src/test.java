81bb9b8e5d4d99bc5a8190634ad224bb52c298ba
申金旺 <2972520044@qq.com>
2022-04-05 22:01:41 +0800

    4_5

---
+++ 4_5/src/test.java
@@ -0,0 +1,14 @@
+/**
+ * @ClassName $申先生
+ * @Description days
+ * @date 2022/4/5 14:09
+ * @Version 1.0
+ */
+
+public class test {
+    public static void main(String[] args) {
+        boolean flg = true;
+        flg = !flg;
+        flg = !flg;
+    }
+}
