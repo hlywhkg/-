916573db0ac2cfd3ec03aaa9c35ed6e97fa67fec
申金旺 <2972520044@qq.com>
2022-04-15 22:28:06 +0800

    4_15

---
+++ 4_15/src/test.java
@@ -0,0 +1,14 @@
+/**
+ * @ClassName $申先生
+ * @Description days
+ * @date 2022/4/15 18:49
+ * @Version 1.0
+ */
+
+public class test {
+    public static void main(String[] args) {
+        char c = 's';
+        String s = c + ""+ "a";
+        System.out.println(s);
+    }
+}
