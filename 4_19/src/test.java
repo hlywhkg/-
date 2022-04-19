1dd19d7e2aaeae634f4d93b960452cffd0fef5d3
申金旺 <2972520044@qq.com>
2022-04-19 23:12:38 +0800

    测试

---
+++ 4_19/src/test.java
@@ -0,0 +1,16 @@
+/**
+ * @ClassName $申先生
+ * @Description days
+ * @date 2022/4/19 18:30
+ * @Version 1.0
+ */
+
+public class test {
+    public static void main(String[] args) {
+        String str = "dsf";
+        String str1 = "agta";
+        String str2 = str1;
+        str1 = str;
+        str = str1;
+    }
+}
