a51a7474c9deb7876d782da9ec7672058bcb05f1
申金旺 <2972520044@qq.com>
2022-04-27 22:29:17 +0800

    4_27

---
+++ 4_27/src/test.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+/**
+ * @ClassName $申先生
+ * @Description days
+ * @date 2022/4/27 18:05
+ * @Version 1.0
+ */
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println(1+"10" + 3+"2");
+        String str ="qwe" + "rty";
+        String str2 = new String(str);
+        System.out.println(str.equals(str2));
+        System.out.println(str == str2);
+        long x = 8234567148193718901l;
+        System.out.println(x);
+        Scanner scanner = new Scanner(System.in);
+        long y = scanner.nextLong();
+    }
+}
