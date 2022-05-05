4552ac68d8b1fd66b4fb752eb373a7eb2d376f29
申金旺 <2972520044@qq.com>
2022-05-05 23:04:20 +0800

    5_5

---
+++ 5_5/src/demo1.java
@@ -0,0 +1,17 @@
+/**
+ * @ClassName $申先生
+ * @Description days
+ * @date 2022/5/5 23:01
+ * @Version 1.0
+ */
+
+public class demo1 {
+    private int count;
+    public static void main(String[] args) {
+        demo1 de = new demo1(88);
+        System.out.println(de.count);
+    }
+    public demo1(int a){
+        count = a;
+    }
+}
