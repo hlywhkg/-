4aa4daeb1718f4a20afff95191a4c88845407b77
申金旺 <2972520044@qq.com>
2022-05-08 22:04:07 +0800

    前端代码

---
+++ 5_6/src/test.java
@@ -0,0 +1,18 @@
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @ClassName $申先生
+ * @Description days
+ * @date 2022/5/6 11:48
+ * @Version 1.0
+ */
+
+public class test {
+    public static void main(String[] args) {
+        Set<Integer> set = new HashSet<>();
+        set.add(10);
+        set.contains()
+    }
+}
