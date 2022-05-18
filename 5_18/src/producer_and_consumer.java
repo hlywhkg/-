e42ed668572cc1dcd105fd1ae91cd626612eb049
申金旺 <2972520044@qq.com>
2022-05-18 23:15:19 +0800

    5_18

---
+++ 5_18/src/producer_and_consumer.java
@@ -0,0 +1,75 @@
+/**
+ * @ClassName $申先生
+ * @Description days
+ * @date 2022/5/18 16:20
+ * @Version 1.0
+ */
+
+public class producer_and_consumer {
+    //产品数量
+    private static Integer count = 0;
+    //缓冲区最大空闲数目
+    private static final Integer FULL = 10;
+    //缓冲池锁
+    private static String LOCK = "lock";
+
+    public static void main(String[] args) {
+        producer_and_consumer test1 = new producer_and_consumer();
+        new Thread(test1.new Producer()).start();
+        new Thread(test1.new Consumer()).start();
+        new Thread(test1.new Producer()).start();
+        new Thread(test1.new Consumer()).start();
+        new Thread(test1.new Producer()).start();
+        new Thread(test1.new Consumer()).start();
+        new Thread(test1.new Producer()).start();
+        new Thread(test1.new Consumer()).start();
+    }
+    class Producer implements Runnable {
+        @Override
+        public void run() {
+            while(true) {
+                try {
+                    Thread.sleep(1000);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                synchronized (LOCK) {
+                    while (count == FULL) {
+                        try {
+                            LOCK.wait();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    count++;
+                    System.out.println(Thread.currentThread().getName() + "生产者生产,目前总共有" + count);
+                    LOCK.notifyAll();
+                }
+            }
+        }
+    }
+    class Consumer implements Runnable {
+        @Override
+        public void run() {
+            while(true){
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                synchronized (LOCK) {
+                    while (count == 0) {
+                        try {
+                            LOCK.wait();
+                        } catch (Exception e) {
+                        }
+                    }
+                    count--;
+                    System.out.println(Thread.currentThread().getName() + "消费者消费，目前总共有" + count);
+                    LOCK.notifyAll();
+                }
+            }
+        }
+    }
+}
+
