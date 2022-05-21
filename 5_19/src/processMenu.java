d7014259a35ecd21b2c350970c2b0a17677d1799
申金旺 <2972520044@qq.com>
2022-05-21 21:57:39 +0800

    操作系统算法\优化哲学家进餐\优化读者写者\优先权算法\优化rr算法

--- 5_19/src/processMenu.java
+++ 5_19/src/processMenu.java
@@ -10,6 +10,40 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.LinkedList;
 
+class JCB {
+    String name;//进程名
+    int arriveTime;//到达时间
+    int serveTime;//服务时间
+    int beginTime;//开始时间
+    int finishTime;//完成时间
+    double priority;//优先权等级
+    int roundTime;//周转时间
+    double aveRoundTime;//带权周转时间
+    double clock = 0;//在时间轮转调度算法中，记录该进程真实服务时间已经用时的时长
+    int waitTime;//记录每个进程到达后的等待时间，只用于最高响应比优先调度算法中
+    boolean firstTimeTag = false;//在RR算法中标识开始时间是否第一次计算
+
+    public JCB() {
+
+    }
+
+    public JCB(String name, int arriveTime, int serveTime, double priority) {
+        super();
+        this.name = name;
+        this.arriveTime = arriveTime;
+        this.serveTime = serveTime;
+        this.waitTime = 0;
+        this.priority = priority;
+    }
+
+    public String toString() {
+        String info = new String("进程名：P" + this.name);
+        return info;
+    }
+
+}
+
+
 public class processMenu {
 
     ArrayList<JCB> jcb;// 存放所有进程
@@ -21,18 +55,21 @@ public class processMenu {
         jcb = new ArrayList<JCB>();
         link = new LinkedList<JCB>();
         new_jcb = new ArrayList<JCB>();
-        JCB p1 = new JCB("1", 0, 4,3);
-        JCB p2 = new JCB("2", 1, 3,2);
-        JCB p3 = new JCB("3", 2, 5,3);
-        JCB p4 = new JCB("4", 3, 2,1);
-        JCB p5 = new JCB("5", 4, 4,5);
+        JCB p1 = new JCB("1", 0, 3,3);
+        JCB p2 = new JCB("2", 2, 6,2);
+        JCB p3 = new JCB("3", 4, 4,3);
+        JCB p4 = new JCB("4", 6, 5,1);
+        JCB p5 = new JCB("5", 8, 2,5);
         jcb.add(p1);jcb.add(p2);jcb.add(p3);jcb.add(p4);jcb.add(p5);
         //先将jcb排序，便于下面的算法实现，就不需要再定义一个标识进程是否已到达的boolean,即无需每次都从头开始扫描jcb容器，
         //而是用一个K记录下当前已经扫描到的位置，一次遍历即可，提高了算法效率。
         Collections.sort(jcb, new compareAt_St());
     }
 
-    public void FCFS(){//先来先服务算法
+    /**
+     * 先来先服务算法
+     */
+    public void FCFS(){
         ProcessQueue pq=new ProcessQueue();//调用内部类
         pq.EnqueueLast();//让最先到达的进程先入队
         System.out.println("*****************************************************");
@@ -42,7 +79,11 @@ public class processMenu {
             pq.EnqueueLast();//已到达的进程入队
         }
     }
-    public void SJF() {// 短作业优先算法
+
+    /**
+     * 短作业优先算法
+     */
+    public void SJF() {
         ProcessQueue pq=new ProcessQueue();
         pq.EnqueueLast();
         System.out.println("*****************************************************");
@@ -53,17 +94,25 @@ public class processMenu {
             Collections.sort(link, new compareSt());//队列中的进程还需按服务时间长度进行排序
         }
     }
-    public void RR() {//时间片轮转调度算法
+
+    /**
+     * 时间片轮转调度算法
+     */
+    public void RR() {
         ProcessQueue pq=new ProcessQueue();
         pq.EnqueueLast();
         System.out.println("*****************************************************");
         while(!link.isEmpty()) {
             pq.DisplayQueue();//打印当前队列中的进程
-            pq.Dequeue(1);//出队，一次一个，因为上一轮出的得让刚到达的进程先进队列，所以没办法，进队操作只能也放在这个函数里了
+            pq.Dequeue(1);//出队，一次一个，因为上一轮出的得让刚到达的进程先进队列，进队操作只能也放在这个函数里了
         }
     }
-    public void HRN() {//最高响应比优先调度算法
-        ProcessQueue pq=new ProcessQueue();
+
+    /**
+     * //最高响应比优先调度算法
+     */
+    public void HRN() {
+        ProcessQueue pq = new ProcessQueue();
         pq.EnqueueLast();
         System.out.println("*****************************************************");
         while(!link.isEmpty()) {
@@ -73,6 +122,20 @@ public class processMenu {
             Collections.sort(link, new comparePriority());//队列中的进程还需按响应比进行排序
         }
     }
+
+    /**
+     * 优先权调度算法
+     */
+    public void priority(){
+        ProcessQueue pq=new ProcessQueue();
+        pq.EnqueueLast();
+        System.out.println("*****************************************************");
+        while(!link.isEmpty()) {
+            pq.DisplayQueue();//打印当前队列中的进程
+            pq.Dequeue(1);//出队，一次一个，因为上一轮出的得让刚到达的进程先进队列，进队操作只能也放在这个函数里了
+        }
+    }
+
     class ProcessQueue{
         int k = 0;// jcb中的进程遍历时的下标
         int nowTime = 0;// 当前时间
@@ -88,16 +151,7 @@ public class processMenu {
                 }
             }
         }
-        public void EnqueueFirst() {//进程首次入队，可一次进多个,从队首进入
-            while (k < jcb.size()) {//当遍历完jcb中的所有进程时结束
-                if (jcb.get(k).arriveTime <= nowTime) {//已经到达的进程按到达时间先后进入队列
-                    link.addFirst(jcb.get(k));
-                    k++;
-                } else {
-                    break;//如果该进程还未入队，即先结束遍历，保留当前下标k值，注意：此处不要k--；
-                }
-            }
-        }
+
         public void Dequeue() {//进程出队，一次只出一个
             nowProcess = link.removeFirst();//移除队列的队首元素并且返回该对象元素
             nowProcess.beginTime = nowTime;//计算开始时间，即为上一个进程的结束时间
@@ -106,11 +160,13 @@ public class processMenu {
             nowProcess.aveRoundTime = (double) nowProcess.roundTime / nowProcess.serveTime;//计算带权周转时间
             nowTime = nowProcess.finishTime;//获得结束时间，即当前时间，方便判断剩下的进程是否已到达
             new_jcb.add(nowProcess);//经处理过数据后加入new_jcb容器
-            for(int i=0;i<link.size();++i) {
-                link.get(i).waitTime++;//所有进入等待队列的进程等待时间+1,此处只为最高响应比算法所用
+            for(int j=0;j<link.size();++j) {
+                link.get(j).waitTime = nowTime - link.get(j).arriveTime;//所有进入等待队列的进程等待时间重新赋值,此处只为最高响应比算法所用
             }
         }
-        public void Dequeue(double sliceTime) {//重载Dequeue方法，实现轮转调度算法的出队
+
+
+        public void Dequeue(double sliceTime) {//重载Dequeue方法，实现轮转调度算法的出队及优先权调度算法
             nowProcess = link.removeFirst();//移除队列的队首元素并且返回该对象元素
             if(nowProcess.firstTimeTag==false) {
                 /*轮转调度进程可能会多次反复进出队列，不像FCFS和SJF的进程只会进出一次，所以计算开始时间可以设个标志位，让每个进程在
@@ -118,20 +174,27 @@ public class processMenu {
                 nowProcess.beginTime=nowTime;//进程开始执行的时间
                 nowProcess.firstTimeTag=true;//计算第一次即可，下次无需更新计算
             }
-            nowTime+=sliceTime;//每次出队，用时一个时间片，更新当前时间
-            nowProcess.clock+=sliceTime;//更新当前出队列的进程已服务时间
+            nowTime += sliceTime;//每次出队，用时一个时间片，更新当前时间
+            nowProcess.clock += sliceTime;//更新当前出队列的进程已服务时间
+            nowProcess.priority -= sliceTime * 2.5;//进程在占用cpu时,会以1.5倍速下降优先权
             if(nowProcess.clock>=nowProcess.serveTime) {
                 nowProcess.finishTime=nowTime;//计算该进程完成时间
                 nowProcess.roundTime = nowProcess.finishTime - nowProcess.arriveTime;//计算周转时间
                 nowProcess.aveRoundTime = (double) nowProcess.roundTime / nowProcess.serveTime;//计算平均周转时间
                 new_jcb.add(nowProcess);//经处理过数据后加入new_jcb容器
-                EnqueueFirst();//已到达的进程先入队
+                EnqueueLast();//已到达的进程先入队
+                Collections.sort(link,new compare_AtPr());
             }
             else {
-                EnqueueFirst();//已到达的进程先入队
+                EnqueueLast();//已到达的进程先入队
                 link.addLast(nowProcess);//上一轮出的再紧接着进入队尾
+                Collections.sort(link,new compare_AtPr());
+            }
+            for(int i = 0 ; i < link.size() ; i++) {//处于等待状态的进程以1倍速增长优先权等级
+                link.get(i).priority += sliceTime;
             }
         }
+
         public void DisplayQueue(){//队列中等候的进程
             i++;
             System.out.println("第"+i+"次队列中排队的进程："+link);
@@ -165,22 +228,43 @@ class compareAt_St implements Comparator<JCB> {// 按到达时间升序，若到
             return -1;
     }
 }
-class comparePriority implements Comparator<JCB>{//按响应比升序排序
+
+class comparePriority implements Comparator<JCB>{//按响应比降序排序
 
     public int compare(JCB o1, JCB o2) {
         double r1=(double)o1.waitTime/o1.serveTime;
         double r2=(double)o2.waitTime/o2.serveTime;
-        return r1>r2?1:-1;
+        return r1 < r2 ? 1 :-1;
     }
 
 }
+
+class compare_AtPr implements Comparator<JCB>{//按优先权降序排序,如果优先权相同,按照先来先到,在就是服务时间
+    public int compare(JCB o1, JCB o2) {
+        if(o1.priority < o2.priority) {
+            return 1;
+        }else if(o1.priority > o2.priority){
+            return -1;
+        }else {
+            int a = o1.arriveTime - o2.arriveTime;
+            if (a > 0)
+                return 1;
+            else if (a == 0) {
+                return o1.serveTime > o2.serveTime ? 1 : -1;
+            } else
+                return -1;
+        }
+    }
+}
+
 class TestProcess {
     public static void main(String[] args) {
         processMenu pm=new processMenu();
         pm.init();//初始化容器
         pm.FCFS();pm.printProcess();
         pm.SJF();pm.printProcess();
-        pm.RR();pm.printProcess();
+        //pm.RR();pm.printProcess();
         pm.HRN();pm.printProcess();
+        pm.priority();pm.printProcess();
     }
 }
