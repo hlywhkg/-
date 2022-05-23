61c7e0db4bc5fd87f085e93cc38f4d96718dc142
申金旺 <2972520044@qq.com>
2022-05-23 23:20:05 +0800

    银行家算法完成~

--- 5_22/src/Banker2.java
+++ 5_22/src/Banker2.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
 
 public class Banker2 {
     //允许的最大进程数量和资源种类
-    final int MAX = 100;
+    final static int MAX = 100;
     //系统中每种资源的总量
     int[] Resource = new int[MAX];
     //没有分配的每种资源总量
@@ -18,19 +18,31 @@ public class Banker2 {
     int[][] Need = new int[MAX][MAX];
     //当前分配情况
     int[][] Allocation = new int[MAX][MAX];
+    //进程对资源的请求
+    int Request[] = new int[MAX];
+    //已经不占用资源的进程
+    int finish[] = new int[MAX];
+    //进程还需要的资源个数
+    int[][] NeedR = new int[MAX][MAX];
+    //存放安全序列
+    int Security[] = new int[MAX];
 
     Scanner sc = new Scanner(System.in);
 
+    //进程的数量M,资源的数量N,当前申请资源的进程I
+    static int M,N,I;
     public void init() {
         boolean flg;
         System.out.println("请输入资源的种类数量");
-        int N = sc.nextInt();
+        N = sc.nextInt();
         System.out.println("请输入各个资源的最大数量Resource[]");
         for (int i = 0; i < N; i++) {
-            Resource[i] = sc.nextInt();
+            int x = sc.nextInt();
+            Resource[i] = x;
+            Available[i] = x;
         }
         System.out.println("请输入进程的数量");
-        int M = sc.nextInt();
+        M = sc.nextInt();
         System.out.println("请输入各个进程所需的各个资源最大数量Need[]");
         do {
             flg = false;
@@ -56,7 +68,8 @@ public class Banker2 {
                     if(Allocation[i][j] > Resource[j]){
                         flg = true;
                     }
-                    Available[j] = Resource[j] - Allocation[i][j];
+                    Available[j] -= Allocation[i][j];
+                    NeedR[i][j] = Need[i][j] - Allocation[i][j];
                 }
             }
             if(flg) {
@@ -66,31 +79,239 @@ public class Banker2 {
     }
 
     //显示资源分配矩阵
-    public void showData(int m,int n){
+    public void showData(){
         int count = 1;
         System.out.println("***********************************");
-        System.out.println("系统目前可用资源Available[]");
-        for (int i = 0; i < n; i++) {
-            System.out.printf("%-10d r",count);
-        }
-        System.out.println();
-        for (int i = 0; i < n; i++) {
-            System.out.printf("%-10d",Available[i]);
-        }
-
-        System.out.println();
         System.out.println("系统当前所有矩阵情况如下:");
-        String a = "Max",b = "Allocation",c = "Need",d = "进程名";
-        System.out.printf("%-12s\t%-12s\t%-12s\t",a,b,c);
-        System.out.printf("%-6s",d);
+        String a = "Available[]",b = "Allocation[]",c = "Need[]";
+        System.out.printf("      %-9s\t",a);
+        System.out.println();
+        System.out.print("资源");
+        for (int j = 0; j < N; j++) {
+            System.out.printf(" r%-1d\t ",count);
+            count++;
+        }
+        System.out.println();
+        for (int i = 0; i < N; i++) {
+            System.out.printf("      %-1d\t",Available[i]);
+        }
 
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < n; j++) {
-                System.out.printf("%-4d\t r",count);
+        System.out.println();
+        System.out.printf("      %-9s\t",b);
+        System.out.println();
+        System.out.print("资源 ");
+        count = 1;
+        for (int j = 0; j < N; j++) {
+            System.out.printf("r%-1d\t ",count);
+            count++;
+        }
+        count = 1;
+        System.out.println();
+        for (int i = 0; i < M; i++) {
+            System.out.printf("p%-2d\t ",count);
+            count++;
+            for (int j = 0; j < N; j++) {
+                System.out.printf("%-1d\t",Allocation[i][j]);
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+        System.out.printf("      %-9s\t",c);
+        System.out.println();
+        System.out.print("资源 ");
+        count = 1;
+        for (int j = 0; j < N; j++) {
+            System.out.printf("r%-1d\t ",count);
+            count++;
+        }
+        System.out.println();
+        count = 1;
+        for (int i = 0; i < M; i++) {
+            System.out.printf("p%-2d\t ",count);
+            count++;
+            for (int j = 0; j < N; j++) {
+                System.out.printf("%-1d\t",Need[i][j]);
+            }
+            System.out.println();
+        }
+
+    }
+
+    //分配资源
+    public int test()
+    {
+        int count = 0;
+        for(int j = 0 ; j < N ; j++)
+        {
+            Available[j] = Available[j] - Request[j];
+            Allocation[I-1][j] += Request[j];
+            NeedR[I-1][j] -= Request[j];
+            if(NeedR[I-1][j] == 0){
+                count++;
+            }
+        }
+        if(count == N){
+            RTest();
+            return 1;
+        }else {
+            return 0;
+        }
+    }
+
+    //释放资源
+    public void RTest(){
+        for (int j = 0; j < N; j++) {
+            Available[j] += Allocation[I-1][j];
+            Allocation[I-1][j] = 0;
+            Need[I-1][j] = 0;
+        }
+    }
+
+    //进程请求资源
+    public boolean RequestR(){
+        int j,count = 1;
+        System.out.println("请输入请求分配资源的进程号(1~"+(M)+"):");
+        I = sc.nextInt();
+        System.out.println("请输入进程P"+I+"要申请的资源个数:");
+        for(j = 0 ; j < N ; j++)
+        {
+            System.out.print("r" + count + ":");
+            count++;
+            Request[j]=sc.nextInt();//输入需要申请的资源
+        }
+        char result = CheckReasonable();
+        if(result == 'Y') {
+            System.out.println("请求分配成功");
+            int ret = test();
+            if(ret == 1){
+                System.out.println("p" + I + "运行完毕");
+            }
+            showData();
+            return true;
+        }else if(result == 'A'){
+            System.out.println("进程p" + I + "申请的资源大于系统空闲资源");
+            System.out.println("分配不合理，不予分配！");
+            return false;
+        }else if(result == 'N') {
+            System.out.println("进程p" + I + "申请的资源大于自身需求资源");
+            System.out.println("分配不合理，不予分配！");
+            return false;
+        }else{
+            System.out.println("不予分配");
+        }
+        return false;
+    }
+
+    //检查分配资源是否合理
+    public char CheckReasonable(){
+        boolean flg = CheckedSafe();
+        if(!flg){
+            return 'U';
+        }
+        for (int i = 0; i < N; i++) {
+            if(Request[i] > Available[i]){
+                return 'A';
+            }else if(Request[i] > NeedR[I-1][i]) {
+                return 'N';
+            }
+        }
+        return 'Y';
+    }
+
+    //安全算法检查,检查是否安全
+    public boolean CheckedSafe(){
+        //初始情况所有资源应该是已经分配过资源
+        //0,代表还占用者.1,代表不在占用
+        int[] work = new int[N];
+        for (int i = 0; i < N; i++) {
+            work[i] = Available[i];
+        }
+        for (int i = 0; i < M; i++) {
+            finish[i] = 0;
+        }
+        int apply,count = 0;
+        for(int i = 0 ;i < M ; i++)
+        {
+            apply=0;
+            for(int j = 0 ; j < N ; j++)
+            {
+                if(finish[i] == 0 && NeedR[i][j] <= work[j])
+                {
+                    apply++;//直到每类资源尚需数都小于系统可利用资源数才可分配
+                    if(apply == N)
+                    {
+                        for(int k = 0 ; k < N ; k++)
+                        {
+                            work[k] += Allocation[i][k];//更改当前可分配资源
+                        }
+                        finish[i] = 1;
+                        Security[count++] = i + 1;
+                        i = -1; //保证每次查询均从第一个进程开始
+                    }
+                }
+            }
+        }
+        for(int i = 0 ; i < M ; i++)
+        {
+            if(finish[i] == 0)
+            {
+                System.out.println("系统不安全！");
+                return false;
+            }
+        }
+        System.out.println("系统安全！");
+        System.out.println("存在一个安全序列：");
+        for(int i = 0 ; i < M;i++)
+        {
+            System.out.print("P"+Security[i]);
+            if(i < M - 1)
+            {
+                System.out.print("->");
             }
         }
         System.out.println();
-
+        return true;
     }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Banker2 banker2 = new Banker2();
+        String choice;
+        System.out.println("*************************************************************");
+        System.out.println("                                                       银行家算法的实现  ");
+        System.out.println("*************************************************************");
+        banker2.init();
+        banker2.showData();
+
+        if(!banker2.CheckedSafe()) {
+            System.out.println("系统不存在安全序列");
+            System.exit(0);
+        }
+
+        do
+        {
+            System.out.println("*************************************************************");
+            System.out.println("                     R(r):请求分配 ");
+            System.out.println("                     E(e):退出        ");
+            System.out.println("*************************************************************");
+            System.out.print("请选择：");
+            choice = sc.nextLine();
+            switch(choice)
+            {
+                case "r":
+                case "R":
+                    banker2.RequestR();
+                    break;
+                case "e":
+                case "E":
+                    System.exit(0);//System.exit(0)是正常退出程序，System.exit(1)表示非正常退出程序。
+                default:
+                    System.out.println("请正确选择！");
+                    break;
+            }
+        }while(choice!="");
+    }
+
 
 }
