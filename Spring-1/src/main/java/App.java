40c4de64041cb84173c175011183b6251499a8ae
申金旺 <2972520044@qq.com>
2022-07-29 22:53:00 +0800

    第一个spring项目

---
+++ Spring-1/src/main/java/App.java
@@ -0,0 +1,12 @@
+/**
+ * @ClassName $申先生
+ * @Description days
+ * @date 2022/7/29 22:25
+ * @Version 1.0
+ */
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("你好,spring");
+    }
+}
